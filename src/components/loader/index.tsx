8d06fe1f407997389dfc37451d031f09c1cbbb7f
MateoSRQ <1Ltseosb>
2020-01-23 17:00:16 -0500

    wqw

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import style from './index.module.css'
+import log from 'loglevel';
+import { Spin, Icon } from 'antd';
+
+import 'antd/dist/antd.css';
+
+//log.setDefaultLevel('info')
+
+interface Props {
+    size?: number,
+    tip?: string,
+    duration: number,
+    children?: any,
+    status: string,
+    auto?: number,
+}
+
+interface State {
+    status: string
+}
+
+
+export default class Component extends React.Component<Props, State> {
+    constructor(props: Props) {
+        log.info('Loader:constructor reached');
+        super(props);
+        this.state = {
+            status: this.props.status
+        };
+
+    }
+
+    componentDidMount(): void {
+        log.info('Loader:componentDidMount reached');
+        if (this.props.auto != 0) {
+            setTimeout(() => {
+                this.setState({
+                    status: 'loaded'
+                })
+            }, this.props.auto)
+        }
+    }
+
+    componentDidUpdate(prevProps: any) {
+        log.info('Loader:componentDidUpdate reached');
+        if (this.props.status !== prevProps.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
+    render() {
+        log.debug('Loader:render reached');
+        let loaded = (this.state.status == 'loaded')?style.loaded:null;
+        let unloaded = (this.state.status == 'loaded')?style.unloaded:null;
+
+        return (
+            <div className={[style.component].join(' ')}>
+                <div className={[style.container, unloaded].join(' ')}>
+                    {this.props.children}
+                </div>
+                <div className={[style.container, loaded].join(' ')}>
+                   <div className={[style.loaderContainer].join(' ')}>
+                       <Spin indicator={<Icon type="loading" style={{ fontSize: this.props.size }} spin />} tip={this.props.tip} />
+                   </div>
+                </div>
+            </div>
+        );
+    }
+    static defaultProps = {size: 24, tip: '', duration: 700, status: 'loading', auto: 0};
+}
+
+
+
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import style from './index.module.css';
+import log from 'loglevel';
+
+import Split from 'react-split-pane';
+import {Icon as KitIcon} from 'react-icons-kit';
+import {menu} from 'react-icons-kit/feather/menu';
+import {ic_menu} from 'react-icons-kit/md/ic_menu';
+
+import Menu from '../../components/menu';
+
+import './resizer.css';
+
+interface State {
+    leftPaneWidth: number
+}
+
+interface Props {
+}
+
+interface Props {}
+export default class Component extends React.Component<Props, State> {
+
+
+    constructor(props: Props) {
+        log.info('Main:constructor reached');
+        super(props);
+        this.handleMenuClick = this.handleMenuClick.bind(this);
+        this.state = {
+            leftPaneWidth: 320
+        };
+    }
+
+    handleMenuClick(): void {
+        log.info('Main:handleMenuClick reached');
+        console.log(this.state.leftPaneWidth)
+        if (this.state.leftPaneWidth == 320) {
+            this.setState({leftPaneWidth: 50});
+        }
+        else {
+            this.setState({leftPaneWidth: 320});
+        }
+
+    }
+
+    componentDidMount(): void {
+        log.info('Main:componentDidMount reached');
+
+    }
+
+    render() {
+        log.info('Main:render reached');
+        return (
+            <div className={[style.component].join(' ')}>
+                <div className={[style.title].join(' ')}>
+                    <div className={[style.menuIcon].join(' ')}>
+                        <div className={[style.icon].join(' ')}>
+                            <KitIcon size={'100%'} icon={ic_menu} onClick={this.handleMenuClick}/>
+                        </div>
+                    </div>
+                </div>
+                <Split minSize={this.state.leftPaneWidth} maxSize={320} split="vertical" className={[style.body].join(' ')}>
+                    <div className={[style.left].join(' ')}>
+                        <Menu />
+                    </div>
+                    <Split  minSize={50} maxSize={320}  className={[style.center].join(' ')}>
+                        <div className={[style.middle].join(' ')}></div>
+                        <div className={[style.right].join(' ')}></div>
+                    </Split>
+                </Split>
+            </div>
+        );
+    }
+}
+
+
+

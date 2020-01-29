0fe954842cb6d3af726901a52c56f6b032ab83f4
Administrator <sanroman_me@hotmail.com>
2020-01-29 00:08:34 -0500

    MANY MORE THINGS

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -14,24 +14,58 @@ interface InputProps {
     placeholder?: string,
     [x: string]: any
 }
-interface InputState {}
+interface InputState {
+    validateStatus?: any
+    result?: string
+}
 
 class NInput extends React.Component<InputProps, InputState> {
     constructor(props: InputProps) {
         log.info('Form.Input:constructor reached');
         super(props);
+        this.onBlur = this.onBlur.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            validateStatus: undefined,
+            result: ''
+        };
+    }
+
+    onBlur(e: any) {
+        console.log('item blur');
+        console.log(this.props.validators)
+        if (this.props.validators) {
+            let result = validate.single(e.currentTarget.value, this.props.validators);
+            if (result == undefined ) {
+                console.log('ok')
+                this.setState({result: result, validateStatus: 'success'});
+            }
+            else {
+                console.log('error')
+                this.setState({result: result, validateStatus: 'warning'});
+            }
+        }
+//        this.props.handleBlur(e);
+    }
+
+    onChange(e: any) {
+        console.log('item change');
+        this.props.handleChange(e);
     }
 
     render() {
         log.info('Form.Input:constructor reached');
-        let {name, label, validateStatus, ...props} = this.props;
+        let {name, label, handleBlur, handleChange, ...props} = this.props;
         return (
             <Form.Item
                 label={label?label:null}
-                validateStatus={validateStatus}
+                validateStatus={this.state.validateStatus}
+                help={this.state.result}
             >
                 <Input
                     name={name}
+                    onBlur={this.onBlur}
+                    onChange={this.onChange}
                     {...props}
                 />
             </Form.Item>
@@ -64,21 +98,15 @@ class NForm extends React.Component<Props, State> {
 
     handleBlur(e: any): void {
         log.info('Form:handleChange reached');
+        console.log('form')
         if (this.props.validators && this.props.validators[e.currentTarget.name]) {
             let result = validate.single(e.currentTarget.value, this.props.validators[e.currentTarget.name]);
-
-            /*
-            this.props.handleChange({
-                index: e.currentTarget.name,
-                value: e.currentTarget.value,
-                result: result
-            })
-            */
         }
     }
 
     handleChange(e: any): void {
         log.info('Form:handleChange reached');
+        console.log('form')
         this.props.handleChange({
             index: e.currentTarget.name,
             value: e.currentTarget.value
@@ -109,14 +137,20 @@ class NForm extends React.Component<Props, State> {
     render() {
         log.debug('Form:render reached');
         let children = null;
+        console.log('RENDER')
+
         if (this.props.children) {
             children = (Array.isArray(this.props.children))?this.props.children:[this.props.children];
             children = children.map((child: any) => {
+
+                console.log(this.props.validators)
+                console.log(this.props.validators[child.props.name])
+
                 let elem = React.cloneElement(child, {
                     key: child.props.name,
-                    onChange: this.handleChange,
-                    onBlur: this.handleBlur,
-                    validations: this.props.validators[child.props.name]
+                    handleChange: this.handleChange,
+                    handleBlur: this.handleBlur,
+                    validators: this.props.validators[child.props.name]
                 })
                 return elem;
             })
